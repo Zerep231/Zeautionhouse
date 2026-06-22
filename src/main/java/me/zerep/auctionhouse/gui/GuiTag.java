4c0666c49de39f68a91019f03ecb6ac401b6f9a2
Zerep231 <101239365+Zerep231@users.noreply.github.com>
2026-06-23 02:17:05 +0700

    unzipgit

---
+++ src/main/java/me/zerep/auctionhouse/gui/GuiTag.java
@@ -0,0 +1,15 @@
+package me.zerep.auctionhouse.gui;
+
+public enum GuiTag {
+    BROWSE,
+    MINE,
+    DELIVERY,
+    CREATE_1,
+    CREATE_2,
+    SHOP_HOME,
+    SHOP_CAT,
+    SHOP_ITEM,
+    SHOP_CONFIRM;
+
+    public static final String HOLDER_PREFIX = "AH:";
+}
